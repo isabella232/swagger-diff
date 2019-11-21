dcd047bb5057070ef8993c676cd28ade2711b3f7
Eric Marshall <emarshall@hubspot.com>
2019-11-21 16:41:46 -0500

    Remove renderers

--- src/test/java/com/deepoove/swagger/test/SwaggerDiffTest.java
+++ src/test/java/com/deepoove/swagger/test/SwaggerDiffTest.java
@@ -1,6 +1,5 @@
 package com.deepoove.swagger.test;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
@@ -13,8 +12,6 @@ import com.deepoove.swagger.diff.model.ChangedEndpoint;
 import com.deepoove.swagger.diff.model.ChangedExtensionGroup;
 import com.deepoove.swagger.diff.model.ChangedOperation;
 import com.deepoove.swagger.diff.model.Endpoint;
-import com.deepoove.swagger.diff.output.HtmlRender;
-import com.deepoove.swagger.diff.output.MarkdownRender;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -28,7 +25,6 @@ public class SwaggerDiffTest {
   final String SWAGGER_V2_DOC1 = "petstore_v2_1.json";
   final String SWAGGER_V2_DOC2 = "petstore_v2_2.json";
   final String SWAGGER_V2_EMPTY_DOC = "petstore_v2_empty.json";
-  final String SWAGGER_V2_HTTP = "http://petstore.swagger.io/v2/swagger.json";
 
   @Test
   public void testEqual() throws IOException {
@@ -42,19 +38,7 @@ public class SwaggerDiffTest {
     List<Endpoint> newEndpoints = diff.getNewEndpoints();
     List<Endpoint> missingEndpoints = diff.getMissingEndpoints();
     List<ChangedEndpoint> changedEndPoints = diff.getChangedEndpoints();
-    String html = new HtmlRender("Changelog",
-        "http://deepoove.com/swagger-diff/stylesheets/demo.css")
-            .render(diff);
 
-    try {
-      FileWriter fw = new FileWriter(
-          "testNewApi.html");
-      fw.write(html);
-      fw.close();
-
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
     Assert.assertTrue(newEndpoints.size() > 0);
     Assert.assertTrue(missingEndpoints.isEmpty());
     Assert.assertTrue(changedEndPoints.isEmpty());
@@ -67,19 +51,7 @@ public class SwaggerDiffTest {
     List<Endpoint> newEndpoints = diff.getNewEndpoints();
     List<Endpoint> missingEndpoints = diff.getMissingEndpoints();
     List<ChangedEndpoint> changedEndPoints = diff.getChangedEndpoints();
-    String html = new HtmlRender("Changelog",
-        "http://deepoove.com/swagger-diff/stylesheets/demo.css")
-            .render(diff);
 
-    try {
-      FileWriter fw = new FileWriter(
-          "testDeprecatedApi.html");
-      fw.write(html);
-      fw.close();
-
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
     Assert.assertTrue(newEndpoints.isEmpty());
     Assert.assertTrue(missingEndpoints.size() > 0);
     Assert.assertTrue(changedEndPoints.isEmpty());
@@ -93,19 +65,6 @@ public class SwaggerDiffTest {
   @Test
   public void testDiff() throws IOException {
     SwaggerDiff diff = SwaggerDiff.compareV2(loadSpec(SWAGGER_V2_DOC1), loadSpec(SWAGGER_V2_DOC2), true);
-    String html = new HtmlRender("Changelog",
-        "src/main/resources/demo.css")
-        .render(diff);
-    
-    try {
-      FileWriter fw = new FileWriter(
-          "testDiff.html");
-      fw.write(html);
-      fw.close();
-      
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
 
     ChangedExtensionGroup tlVendorExts = diff.getChangedVendorExtensions();
     assertVendorExtensionsAreDiff(tlVendorExts);
@@ -122,23 +81,6 @@ public class SwaggerDiffTest {
         assertVendorExtensionsAreDiff(changedOperation);
       }
     }
-//    Assert.assertFalse(changedEndPoints.isEmpty());
-  }
-
-  @Test
-  public void testDiffAndMarkdown() throws IOException {
-    SwaggerDiff diff = SwaggerDiff.compareV2(loadSpec(SWAGGER_V2_DOC1), loadSpec(SWAGGER_V2_DOC2), true);
-    String render = new MarkdownRender().render(diff);
-    try {
-      FileWriter fw = new FileWriter(
-          "testDiff.md");
-      fw.write(render);
-      fw.close();
-
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
   }
 
   private void assertEqual(SwaggerDiff diff) {
